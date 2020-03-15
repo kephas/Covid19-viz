26a2e2cfeea231daa81e7dacf0caa13c80255941
Sheldon <sheldon@datacloud.com>
2020-03-15 17:54:30 +0100

    Draw circles whose radii are proportional to the number of confirmed cases

--- plot_covid_data.py
+++ plot_covid_data.py
@@ -7,14 +7,38 @@ Created on Sun Mar 15 17:01:50 2020
 """
 
 import os 
+import pandas as pd
 import folium
 from folium import plugins
 import rasterio as rio
 from rasterio.warp import calculate_default_transform, reproject, Resampling
 import earthpy as et
-
-m = folium.Map(location=[40.0150, -105.2705],
-              tiles = 'Stamen Terrain')
+import pdb 
 
 
-m.save("./mymap.html")
+#ConfirmedCases_raw=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv')
+#Deaths_raw=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv')
+#Recoveries_raw=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv')
+
+
+map = folium.Map(location=[0,0],
+              tiles = 'Stamen Terrain',
+              zoom_start=3)
+
+
+points = (ConfirmedCases_raw.Lat,ConfirmedCases_raw.Long)
+date_to_analyze='3/14/20'
+radius = ConfirmedCases_raw[date_to_analyze].values.astype('float')
+
+# Setting lat and long 
+lat = points[0]
+long = points[1]
+
+for la,lo,ra in zip(lat[0:500],long[0:500],radius[0:500]):
+    folium.Circle(
+        location=[la,lo],
+        radius=ra*10
+    ).add_to(map)
+
+
+map.save("./mymap.html")
