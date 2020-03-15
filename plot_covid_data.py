4370805df0487388014d3d19366325a7c130e0da
Sheldon <sheldon@datacloud.com>
2020-03-15 18:14:18 +0100

    Create function to plot circles

--- plot_covid_data.py
+++ plot_covid_data.py
@@ -15,30 +15,39 @@ from rasterio.warp import calculate_default_transform, reproject, Resampling
 import earthpy as et
 import pdb 
 
-
+############# LOAD DATAFRAMES
 #ConfirmedCases_raw=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv')
 #Deaths_raw=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv')
 #Recoveries_raw=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv')
 
 
+def plot_number_of_cases(df,date,custom_color):
+     
+#     date_to_analyze='3/14/20'
+     dc=df.iloc[df[date_to_analyze].nonzero()]
+
+     latitude = dc.Lat.values.astype('float')
+     longitude = dc.Long.values.astype('float')
+     radius = dc[date_to_analyze].values.astype('float')
+
+
+
+     for la,lo,ra in zip(latitude,longitude,radius):
+         folium.Circle(
+             location=[la,lo],
+             radius=ra*10,
+             fill=True,
+             color=custom_color,
+             fillColor=custom_color,
+             fillOpacity=0.5
+         ).add_to(map)
+
 map = folium.Map(location=[0,0],
               tiles = 'Stamen Terrain',
               zoom_start=3)
 
-
-points = (ConfirmedCases_raw.Lat,ConfirmedCases_raw.Long)
-date_to_analyze='3/14/20'
-radius = ConfirmedCases_raw[date_to_analyze].values.astype('float')
-
-# Setting lat and long 
-lat = points[0]
-long = points[1]
-
-for la,lo,ra in zip(lat[0:500],long[0:500],radius[0:500]):
-    folium.Circle(
-        location=[la,lo],
-        radius=ra*10
-    ).add_to(map)
+plot_number_of_cases(ConfirmedCases_raw,'3/14/20','blue')
+plot_number_of_cases(Deaths_raw,'3/14/20','red')
 
 
 map.save("./mymap.html")
