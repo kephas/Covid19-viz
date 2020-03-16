15a1211c7c2a82bf441ccf351918c30b7c6c861e
Ovide19 <62220493+Ovide19@users.noreply.github.com>
2020-03-16 11:23:09 +0100

    Bind port test
    From https://stackoverflow.com/questions/22002440/heroku-app-crashes-immediately-with-r10-and-h10-errors

--- plot_covid_data.py
+++ plot_covid_data.py
@@ -91,6 +91,8 @@ covid_data.plot_number_of_cases_for_all_dataframes(my_date)
 #covid_data.map.save("./mytest.html")
 app = Flask(__name__)
 @app.route("/")
+port = int(os.environ.get("PORT", 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
 def display_map():
      return covid_data.map._repr_html_()
 
